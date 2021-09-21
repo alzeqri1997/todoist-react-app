66e2b5b1798dc456541392f53f39f4306d063c3a
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-21 15:18:20 +0300

    add userProject hook

---
+++ src/hooks/index.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import moment from 'moment';
+import { firebase } from '../firebase';
+
+export const useProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('projects')
+      .where('userId', '==', '5730d432-ecaa-4f4f-ba1e-c283731f0ab4')
+      .orderBy('projectId')
+      .get()
+      .then((snapshot) => {
+        const allProjects = snapshot.docs.map((project) => ({
+          ...project.data(),
+          docId: project.id,
+        }));
+
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects);
+        }
+      });
+  }, [projects]);
+
+  return { projects, setProjects };
+};
