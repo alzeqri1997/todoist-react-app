5328cec01574db73541b8ec1b98478316a7d7d6f
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-22 06:15:39 +0300

    add selectedProjectProvider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import { Header } from './components/layout/Header';
-import { ProjectProvider } from './context';
+import { ProjectProvider, SelectedProjectProvider } from './context';
 // import { addFirebaseTask } from './firebase';
 
 function App({ darkModeDefault = false }) {
   const [darkMode, setDarkMode] = useState(darkModeDefault);
   return (
-    <ProjectProvider>
-      <main className={darkMode ? 'darkmode' : undefined}>
-        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-      </main>
-    </ProjectProvider>
+    <SelectedProjectProvider>
+      <ProjectProvider>
+        <main className={darkMode ? 'darkmode' : undefined}>
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+        </main>
+      </ProjectProvider>
+    </SelectedProjectProvider>
   );
 }
 
