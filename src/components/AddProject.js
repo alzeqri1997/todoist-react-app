2d00b6948dbacdfdee56680aabd9b70cee889d06
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-24 17:28:10 +0300

    AddProject component added

---
+++ src/components/AddProject.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { firebase } from '../firebase';
+import { generatePushId } from '../helpers';
+import { useProjectValue } from '../context';
+
+export const AddProject = ({ shouldShow = false }) => {
+  const [show, setShow] = useState(shouldShow);
+  const [projectName, setProjectName] = useState('');
+
+  const projectId = generatePushId();
+  const { projects, setProjects } = useProjectValue();
+
+  const addProject = () =>
+    projectName &&
+    firebase
+      .firestore()
+      .collection('projects')
+      .add({
+        projectId,
+        name: projectName,
+        useId: '5730d432-ecaa-4f4f-ba1e-c283731f0ab4',
+      })
+      .then(() => {
+        setProjects([...projects]);
+        setProjectName('');
+        setShow(false);
+      });
+
+  return (
+    <div className="add-project" data-testid="add-project">
+      {show && (
+        <div className="add-project__input" data-testid="add-project-inner">
+          <input
+            type="text"
+            value={projectName}
+            onChange={(e) => setProjectName(e.target.value)}
+            className="add-project__name"
+            data-testid="project-name"
+            type="text"
+            placeholder="Name your project"
+          />
+
+          <button
+            className="add-project__submit"
+            type="button"
+            onClick={() => addProject()}
+            data-testid="add-project-submit"
+          >
+            Add Project
+          </button>
+
+          <span
+            arie-label="Cancel adding project"
+            data-testid="hide-project-overlay"
+            className="add-project-cancel"
+            onClick={() => setShow(false)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') setShow(false);
+            }}
+            role="button"
+            tabIndex={0}
+          >
+            Cancel
+          </span>
+        </div>
+      )}
+      <span
+        aria-label="Add Project"
+        data-testid="add-project-action"
+        className="add-project__text"
+        onClick={() => setShow(!show)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') setShow(!show);
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        <span className="add-project__text--plus">+</span>
+        Add Project
+      </span>
+    </div>
+  );
+};
+
+AddProject.propTypes = {
+  shouldShow: PropTypes.bool,
+};
